6bd7f2f213d77ae36af086580b7c03bee54b1128
GuanXL :) <42903364+TeFuirnever@users.noreply.github.com>
2020-01-18 15:39:41 +0800

    Add files via upload

---
+++ C++primer5课后题/Chapter 6/6.13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void a(int);
+void b(int&);
+int main(){
+	int s = 0, t = 10;
+	a(s);
+	cout << s << endl;
+	b(t);
+	cout << t << endl;
+
+	system("pause");
+	return 0;
+}
+void a(int i){
+	++i;
+	cout << i << endl;
+}
+void b(int& j){
+	++j;
+	cout << j << endl;
+}
---
+++ C++primer5课后题/Chapter 6/6.4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int fact(int i)
+{
+	if (i < 0)
+		return -1;
+	int sum = 0;
+	sum = i > 1 ? i * fact(i - 1) : 1;
+
+	return sum;
+}
+int main()
+{
+	int val, sum = 0;
+	cin >> val;
+	cout << val << " µÄ½×³ËÊÇ " << fact(val) << endl;
+
+	system("pause");
+	return 0;
+}
